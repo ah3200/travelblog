7a0db97b86738811f2e7c76cc97f669176d18224
ah3200 <ah3200@nyu.edu>
2016-05-15 18:26:18 +0000

    adjust story list page layout

--- api/serializers.py
+++ api/serializers.py
@@ -3,14 +3,14 @@ from rest_framework import serializers
 from blogengine.models import Story, User
 
 class UserSerializer(serializers.ModelSerializer):
-#    stories = serializers.HyperlinkedIdentityField(view_name='userstory-list', lookup_field='username')
+    #stories = serializers.HyperlinkedIdentityField('stories', view_name='userstory-list', lookup_field='username')
     
     class Meta:
         model = User
         field = ('id','username','email','first_name','last_name')
         
 class StorySerializer(serializers.ModelSerializer):
-    authoer = UserSerializer(required=False)
+    author = UserSerializer(required=False)
     
     def get_validation_exclusions(self):
         exclusions = super(StorySerializer, self).get_validation_exclusions()
@@ -18,4 +18,3 @@ class StorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Story
-        
